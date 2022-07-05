2fb27203380f42154d83833ff032d71aa46f3484
Cmetana <cmeetana@gmail.com>
2022-07-05 21:34:50 +0300

    init: start create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n <= 0) { //check if number are not negative
+        let delMin = -n; //transform number to positive  
+        let splitToArray = delMin.split(''); // split number to array
+        let reverseArray = reverse(splitToArray); // reverse array
+        let joinArray = reverseArray.join(); // join array
+        let result = Number(joinArray);
+    }
+    return result; 
 }
