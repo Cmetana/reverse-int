24d698498d4a2fe827e7256125e7b0544ae2dd80
Cmetana <cmeetana@gmail.com>
2022-07-06 19:16:03 +0300

    fix: already working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
     if (n < 0) { //check if number are not negative
-        let numToPositive = -n;
-        return String.numToPositive.split('').reverse().join('');
+        let numToPositive = -n; 
+        return numToPositive.toString().split('').reverse().join(''); 
     }
-    return String.n.split('').reverse().join('');
+    return n.toString().split('').reverse().join('');
 };
